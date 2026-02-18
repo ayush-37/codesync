8693bf3b66db98f4a61cfd9f827753e6acfa18d3
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-02-18 13:13:52 +0530

    Time: 3 ms (93.11%) | Memory: 8.8 MB (23.74%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/beautiful-arrangement.cpp
@@ -1,24 +1,24 @@
 class Solution {
-private:
-    void arrange(vector<int>& not_used, int ind, int& ans){
-        if(ind > not_used.size()){
-            ans++;
-            return;
-        }
+public:
+    int solve(vector<int>& memo, int msk, int n){
+        if(msk == (1<<n)-1)return 1;
+        if(memo[msk] != -1)return memo[msk];
 
-        for(int i = 0; i < not_used.size(); i++){
-            if((not_used[i] && ((i+1)%ind == 0 || ind%(i+1) == 0))){
-                not_used[i] = 0;
-                arrange(not_used,ind+1,ans);
-                not_used[i] = 1;
+        int pos = __builtin_popcount(msk)+1;
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            if(msk & 1<<i)continue; // already used
+            int num = i+1;
+            if((num%pos == 0) || (pos%num == 0)){
+                ans += solve(memo,msk|(1<<i),n);
             }
         }
+        return memo[msk] = ans;
     }
 public:
     int countArrangement(int n) {
-        vector<int> not_used(n,1);
-        int ans = 0;
-        arrange(not_used,1,ans);
-        return ans;
+        vector<int> memo(1<<n,-1);
+        return solve(memo,0,n);
+
     }
 };
