29c3471072af180984f702137c0550c41b0c0e96
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-04-01 12:21:23 +0530

    Time: 67 ms (32.34%) | Memory: 215.1 MB (27.35%) - LeetSync

--- 2846-robot-collisions/robot-collisions.cpp
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -1,68 +1,58 @@
 class Solution {
 public:
 struct state{
-    int pos, hea;
+    int pos,hea,ind;
     char dir;
 };
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
         int n = positions.size();
         vector<state> v(n);
-        unordered_map<int,state> mp;
         for(int i = 0; i < n; i++){
-            v[i].pos = positions[i];
-            v[i].hea = healths[i];
-            v[i].dir = directions[i];
-
-            mp[positions[i]] = v[i];
+            v[i] = {positions[i],healths[i],i,directions[i]};
         }
 
-        sort(v.begin(),v.end(),[](const state& a, const state& b){
+        sort(v.begin(),v.end(),[] (const state& a, const state& b){
             return a.pos < b.pos;
         });
 
         stack<state> st;
         for(int i = 0; i < n; i++){
-            if(st.empty()){
-                st.push(v[i]);
-            }
+            state curr = v[i];
+            if(curr.dir == 'R')st.push(curr);
             else{
-                if(v[i].dir == 'R'){
-                    st.push(v[i]);
-                }
-                else{
-                    int h = v[i].hea, p = v[i].pos;
-                    char d = v[i].dir;
-                    while(!st.empty() && st.top().dir == 'R' && h > 0){
-                        auto s = st.top();
-                        st.pop();
-                        if(s.hea == h){
-                            mp.erase(s.pos);
-                            mp.erase(p);
-                            h = 0;
-                        }
-                        else if(s.hea < h){
-                            mp.erase(s.pos);
-                            h--;
-                        }
-                        else {
-                            mp.erase(p);
-                            s.hea -= 1;
-                            mp[s.pos] = s;
-                            st.push(s);
-                            h = 0;
-                        }
+                while(!st.empty() && st.top().dir == 'R' && curr.hea > 0){
+                    state prev = st.top();
+                    st.pop();
+                    if(prev.hea == curr.hea){
+                        curr.hea = 0;
+                        prev.hea = 0;
                     }
-                    if(h > 0){
-                        mp[p].hea = h;
-                        st.push(mp[p]);
+                    else if(prev.hea < curr.hea){
+                        prev.hea = 0;
+                        curr.hea -= 1;
+                    }
+                    else{
+                        prev.hea -= 1;
+                        curr.hea = 0;
+                        st.push(prev);
                     }
                 }
+                if(curr.hea > 0)st.push(curr);
             }
         }
 
+        vector<state> temp;
+
+        while(!st.empty()){
+            temp.push_back(st.top());
+            st.pop();
+        }
+        sort(temp.begin(),temp.end(),[] (const state& a, const state& b){
+            return a.ind < b.ind;
+        });
         vector<int> ans;
-        for(int i = 0; i < n; i++){
-            if(mp.count(positions[i]))ans.push_back(mp[positions[i]].hea);
+        for(auto x: temp){
+            ans.push_back(x.hea);
         }
         return ans;
     }
