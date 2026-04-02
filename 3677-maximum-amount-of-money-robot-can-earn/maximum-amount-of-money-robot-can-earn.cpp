389c49878d2a424d7a283e714034ca4fd41cee96
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-04-03 01:23:58 +0530

    Time: 483 ms (61.00%) | Memory: 277.1 MB (23.24%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -2,31 +2,45 @@ class Solution {
 public:
     int maximumAmount(vector<vector<int>>& coins) {
         int n = coins.size(), m = coins[0].size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(m+1,vector<int>(3,INT_MIN)));
+        vector<vector<vector<int>>> dp(
+            n + 1, vector<vector<int>>(m + 1, vector<int>(3, INT_MIN/2)));
         dp[0][1][0] = dp[1][0][0] = 0;
 
-        for(int i = 1; i < n+1; i++){
-            for(int j = 1; j < m+1; j++){
-                int val = coins[i-1][j-1];
-                for(int k = 0; k < 3; k++){
-                    int best = max(dp[i-1][j][k],dp[i][j-1][k]);
-                    if(best == INT_MIN)continue;
-
-                    dp[i][j][k] = max(dp[i][j][k], best + val);
-
-                    if(val < 0 && k < 2){
-                        dp[i][j][k+1] = max(dp[i][j][k+1], best);
-                    }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (coins[i - 1][j - 1] >= 0) {
+                    dp[i][j][0] = max(dp[i][j - 1][0], dp[i - 1][j][0]) +
+                                  coins[i - 1][j - 1];
+                    if (dp[i - 1][j][1] != INT_MIN ||
+                        dp[i][j - 1][1] != INT_MIN)
+                        dp[i][j][1] = max(dp[i - 1][j][1], dp[i][j - 1][1]) +
+                                      coins[i - 1][j - 1];
+                    if (dp[i - 1][j][2] != INT_MIN ||
+                        dp[i][j - 1][2] != INT_MIN)
+                        dp[i][j][2] = max(dp[i - 1][j][2], dp[i][j - 1][2]) +
+                                      coins[i - 1][j - 1];
+                } else {
+                    dp[i][j][0] = max(dp[i][j - 1][0], dp[i - 1][j][0]) +
+                                  coins[i - 1][j - 1];
+                    if (dp[i - 1][j][0] != INT_MIN ||
+                        dp[i][j - 1][0] != INT_MIN ||
+                        dp[i - 1][j][1] != INT_MIN ||
+                        dp[i][j - 1][1] != INT_MIN)
+                        dp[i][j][1] =
+                            max(max(dp[i - 1][j][0], dp[i][j - 1][0]),
+                                max(dp[i - 1][j][1], dp[i][j - 1][1]) +
+                                    coins[i - 1][j - 1]);
+                    if (dp[i - 1][j][1] != INT_MIN ||
+                        dp[i][j - 1][1] != INT_MIN ||
+                        dp[i - 1][j][2] != INT_MIN ||
+                        dp[i][j - 1][2] != INT_MIN)
+                        dp[i][j][2] =
+                            max(max(dp[i - 1][j][1], dp[i][j - 1][1]),
+                                max(dp[i - 1][j][2], dp[i][j - 1][2]) +
+                                    coins[i - 1][j - 1]);
                 }
             }
         }
-
         return max({dp[n][m][0], dp[n][m][1], dp[n][m][2]});
     }
 };
-
-/*
-Complexity
-Time: O(n * m * 3)
-Space: O(n * m * 3)
-*/
