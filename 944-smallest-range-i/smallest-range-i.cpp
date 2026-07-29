0c040714f862b59fc827e9da44801eff801508bf
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-07-29 19:33:34 +0530

    Time: 2 ms (12.75%) | Memory: 19.2 MB (93.38%) - LeetSync

--- 944-smallest-range-i/smallest-range-i.cpp
+++ 944-smallest-range-i/smallest-range-i.cpp
@@ -1,37 +1,10 @@
 class Solution {
 public:
     int smallestRangeI(vector<int>& nums, int k) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
+        int maxi = *max_element(nums.begin(),nums.end())-k;
+        int mini = *min_element(nums.begin(),nums.end())+k;
 
-        stack<pair<int,int>> st;
-        for(int i = 0; i < n; i++){
-            if(st.empty())st.push({nums[i]-k, nums[i]+k});
-            else{
-                int l = nums[i]-k, r = nums[i]+k;
-                while(!st.empty() && st.top().second >= nums[i] - k){
-                    
-                    l = max(st.top().first,nums[i]-k);
-                    r = min(st.top().second,nums[i]+k);
-                    st.pop();
-                }
-                st.push({l,r});
-            }
-            // cout<<st.top().first<<" "<<st.top().second<<endl;
-        }
-
-        int mini1 , maxi1, mini2, maxi2;
-        if(st.size() == 1)return 0;
-        else{
-            mini2 = st.top().first, maxi2 = st.top().second;
-            while(!st.empty()){
-                st.pop();
-                if(st.size() == 1){
-                    mini1 = st.top().first, maxi1 = st.top().second;
-                }
-            }
-
-            return mini2-maxi1;
-        }
+        if(mini >= maxi)return 0;
+        else return maxi-mini;
     }
 };
