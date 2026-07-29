bd38af3c6fbfbf836d2072d81e8ace46c034fbeb
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-07-29 13:58:11 +0530

    Time: 8 ms (21.78%) | Memory: 98.1 MB (69.13%) - LeetSync

--- 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.cpp
+++ 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int partitionDisjoint(vector<int>& nums) {
-        int n = nums.size(), big = nums[0]; 
-        vector<int> small(n,0);
-
-        small[n-1] = nums[n-1];
-        for(int i = n-2; i >= 0; i--){
-            small[i] = min(small[i+1],nums[i]);
-        }
-
-        int ans = 0;
+        int n = nums.size(), curr_big = nums[0], biggest = nums[0];
+        int ans = 1;
         for(int i = 1; i < n; i++){
-            if(big <= small[i]){
-                ans = i;
-                break;
+            if(nums[i] < curr_big){
+                ans = i+1;
+                curr_big = biggest;
             }
-            big = max(big,nums[i]);
+            else{
+                biggest = max(biggest,nums[i]);
+            }
         }
         return ans;
     }
