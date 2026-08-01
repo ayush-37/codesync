8254995439707fb152ac07531648fc6c703c235e
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-08-01 17:54:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (68.21%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+
+/*
+CATCH
+since we have even no of piles and total no of stones odd
+ALice will always win 
+at the start of game she can calculate the sum of odd places and then even places 
+which is better she will start picking it that way
+
+*/
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
