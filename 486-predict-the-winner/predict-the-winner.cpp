d3a9e725ab3e3a3df4bda40df0b8cd7a90350f40
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-08-01 17:33:54 +0530

    Time: 2 ms (43.58%) | Memory: 11.1 MB (7.52%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
+    unordered_map<int, int> memo;
     bool predictTheWinner(vector<int>& nums) {
-        int i = 0, j = nums.size()-1;
-        bool case1 = (nums[i] - solve(nums,i+1,j)) >= 0;
-        bool case2 = (nums[j] - solve(nums,i,j-1)) >= 0;
+        // memo.clear();
+        int n = nums.size();
 
-        return case1|case2;
+        int c1 = nums[0] - solve(nums,1,n-1);
+        int c2 = nums[n-1] - solve(nums,0,n-2);
 
+        return (max(c1,c2) >= 0);
     }
 
     int solve(vector<int>& nums, int i, int j){
         if(i > j)return 0;
+        if(memo.find(i*100 + j) != memo.end())return memo[i*100 + j];
 
         int c1 = nums[i] - solve(nums,i+1,j);
         int c2 = nums[j] - solve(nums,i,j-1);
-        return max(c1,c2);
+        return memo[i*100 + j] = max(c1,c2);
     }
 };
