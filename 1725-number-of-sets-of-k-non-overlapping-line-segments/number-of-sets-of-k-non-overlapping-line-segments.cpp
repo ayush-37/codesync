a3325837802063764dcbff7aa2904e3e39a588fa
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-08-16 16:08:23 +0530

    Time: 59 ms (55.22%) | Memory: 54.5 MB (33.33%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     const int MOD = 1e9+7;  
-    vector<vector<int>> memo;   
+    vector<vector<int>> memo, pref;   
     int numberOfSets(int n, int k) {
         memo.resize(n+1,vector<int>(k+1,-1));
+        pref.resize(n+1,vector<int>(k+1,-1));
         int ans = solve(n,k);
         return ans;
     }
@@ -16,14 +17,16 @@ public:
         if(memo[n][k] != -1)return memo[n][k];
 
         int notTake = solve(n-1,k);
-        int take = 0;
-        int i = n-1;
-        while(i >= 2){
-            take = (take + solve(i,k-1)) % MOD;
-            i--;
-        }
+        int take = sumPref(n-1,k-1);
 
         memo[n][k] = (take + notTake) % MOD;
         return memo[n][k];
+    }
+
+    int sumPref(int n, int k){
+        if(n < 2)return 0;
+        if(pref[n][k] != -1)return pref[n][k];
+        pref[n][k] = (solve(n,k) + sumPref(n-1,k)) % MOD;
+        return pref[n][k];
     }
 };
