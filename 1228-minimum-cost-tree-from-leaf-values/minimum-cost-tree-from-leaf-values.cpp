354fff0b6daa47e95db57c5e10729504cf06cd4d
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-08-18 16:36:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (87.82%) - LeetSync

--- 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    using ll = long long;
     int mctFromLeafValues(vector<int>& arr) {
-        int n = arr.size();
-        vector<vector<ll>> dp(n,vector<ll>(n,-1));
-        return solve(0,n-1,arr,dp);
-    }
-    ll solve(int l, int r, vector<int>& arr, vector<vector<ll>>& dp){
-        if(l == r)return 0;
-        if(dp[l][r] != -1)return dp[l][r];
-
-        ll lm = 0, ans = LLONG_MAX;
-        for(int i = l; i < r; i++){
-            lm = max(lm,arr[i]*1LL);
-            ll rm = 0;
-            for(int j = i+1; j <= r; j++)rm = max(rm, arr[j]*1LL);
-
-            ans = min(ans, lm*rm + solve(l,i,arr,dp) + solve(i+1,r,arr,dp));
+        vector<int> st;
+        int res= 0;
+        for(auto x: arr){
+            while(!st.empty() && st.back() <= x){
+                int y = st.back();
+                st.pop_back();
+                int temp = st.empty()?INT_MAX: st.back();
+                res += y * min(x,temp);
+            }
+            st.push_back(x);
         }
-        return dp[l][r] = ans;
+
+        while(st.size() > 1){
+            int x = st.back();
+            st.pop_back();
+            res += x*st.back();
+        }
+        return res;
     }
 };
