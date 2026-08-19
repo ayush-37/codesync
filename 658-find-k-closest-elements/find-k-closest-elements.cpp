ad713fa2a975b9747aebf488cb2ab77f9eb49847
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-08-19 15:39:37 +0530

    Time: 0 ms (100.00%) | Memory: 35.8 MB (93.30%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -2,28 +2,19 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         int n = arr.size();
-        vector<int> ans;
-        int l = upper_bound(arr.begin(),arr.end(), x) - arr.begin() - 1, r = l+1;
-        while(l >= 0 && r < n && ans.size() != k){
-            if(abs(arr[l] - x) <= abs(arr[r]-x)){
-                ans.push_back(arr[l]);
-                l--;
+        int s = 0, e = n-k;
+        
+        while(s < e){
+            int mid = s + (e-s)/2;
+            if(x-arr[mid] <= arr[mid+k] - x){
+                e = mid;
             }
-            else{
-                ans.push_back(arr[r]);
-                r++;
+            else {
+                s = mid+1;
             }
-        }
-        while(l >= 0 && ans.size() != k){
-            ans.push_back(arr[l]);
-            l--;
-        }
-        while(r < n && ans.size() != k){
-            ans.push_back(arr[r]);
-            r++;
         }
 
-        sort(ans.begin(),ans.end());
-        return ans;
+        vector<int> res(arr.begin()+s, arr.begin()+s+k);
+        return res;
     }
 };
