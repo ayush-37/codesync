7debecfe7d50d8f35b6635924135dfa2e43b332f
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-08-19 13:01:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (89.45%) - LeetSync

--- 654-maximum-binary-tree/maximum-binary-tree.cpp
+++ 654-maximum-binary-tree/maximum-binary-tree.cpp
@@ -12,31 +12,17 @@
 class Solution {
 public:
     TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        int n = nums.size();
-        TreeNode* root = create(0,n-1,nums);
-        return root;
-    }
-
-    TreeNode* create(int s, int e, vector<int>& nums){
-        if(s == e){
-            TreeNode* root = new TreeNode(nums[s]);
-            return root;
-        }
-
-        if(s > e)return NULL;
-
-        int ind = s, maxi = nums[s];
-        for(int i = s; i <= e; i++){
-            if(nums[i] > maxi){
-                maxi = nums[i];
-                ind = i;
+        deque<TreeNode*> dq;
+        for(auto x: nums){
+            TreeNode* curr = new TreeNode(x);
+            while(!dq.empty() && dq.back()->val < x){
+                auto f = dq.back();
+                dq.pop_back();
+                curr->left = f;
             }
+            if(!dq.empty())dq.back()->right = curr;
+            dq.push_back(curr);
         }
-
-        TreeNode* root = new TreeNode(nums[ind]);
-        root->left = create(s,ind-1,nums);
-        root->right = create(ind+1,e,nums);
-
-        return root;
+        return dq.front();
     }
 };
