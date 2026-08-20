2899aaff329502d82e04e35ce711b26e2d2a63d4
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-08-20 15:30:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (99.21%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> mp;
     vector<int> diffWaysToCompute(string expression) {
         int n = expression.size();
-        mp.resize(n,vector<vector<int>>(n));
-        vector<int> ans;
-        ans = solve(expression, 0, n-1);
-        return ans;
-    }
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n));
 
-    vector<int> solve(string s, int l, int r){
-        if(l == r)return {s[l] - '0'};
-        else if(l > r)return {0};
-        else if(r-l == 1 && isdigit(s[l])){
-            int tens = s[l] - '0';
-            int ones = s[r] - '0';
-            return {10 * tens + ones};
+        // base case single digit or two digit number
+        for(int i = 0; i < n; i++){
+            if(isdigit(expression[i])){
+                int x1 = expression[i]-'0';
+                if(i+1 < n && isdigit(expression[i+1])){
+                    int x2 = x1*10 + (expression[i+1]-'0');
+                    dp[i][i+1].push_back(x2);
+                }
+                dp[i][i].push_back(x1);
+            }
         }
 
-        if(!mp[l][r].empty())return mp[l][r];
+        // Fill the dp table for all possible subexpressions
+        for(int len = 3; len <= n; len++){
+            for(int i = 0; i + len -1 < n; i++){
+                int start = i;
+                int end = i + len - 1;
+                for(int j = start; j <= end; j++){
+                    if(isdigit(expression[j]))continue;
+                    // Try all possible positions to split the expression
+                    vector<int> left = dp[start][j-1];
+                    vector<int> right = dp[j+1][end];
 
-        vector<int> temp;
-        for(int i = l; i <= r; i++){
-            if(isdigit(s[i]))continue;
-
-            vector<int> left = solve(s, l, i-1);
-            vector<int> right = solve(s, i+1, r);
-
-            for(auto x: left){
-                for(auto y: right){
-                    if(s[i] == '+')temp.push_back(x+y);
-                    else if(s[i] == '-')temp.push_back(x-y);
-                    else temp.push_back(x*y);
+                    // Compute results based on the operator at position 'split'
+                    for(auto x: left){
+                        for(auto y: right){
+                            if(expression[j] == '+')dp[start][end].push_back(x + y);
+                            else if(expression[j] == '-')dp[start][end].push_back(x - y);
+                            else dp[start][end].push_back(x * y);
+                        }
+                    }
                 }
             }
         }
-        return mp[l][r] = temp;
+
+        return dp[0][n-1];
     }
 };
