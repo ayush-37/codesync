ec8cc94c5c7209854895db3e140a5673f960679d
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-08-20 15:16:50 +0530

    Time: 2 ms (73.51%) | Memory: 10.6 MB (68.72%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -1,65 +1,40 @@
-/*
-Time complexity: O(n⋅2^n)
-
-For each sub-expression, we iterate through the string to identify the operators, which takes O(n) time. However, the key aspect is the recursive combination of results from the left and right sub-expressions. The number of results grows exponentially because each sub-expression produces multiple results, and combining these results takes O(k×l), where k and l are the numbers of results from the left and right sub-problems, respectively.
-
-There were some suggestions to model the number of results using Catalan numbers which we deemed as incorrect. Catalan numbers apply when counting distinct ways to fully parenthesize an expression or structure. In this problem, however, we're not just counting valid ways to split the expression but also calculating and combining all possible results. This introduces exponential growth in the number of possible results, not the polynomial growth typical of Catalan numbers. The number of combinations grows exponentially with the depth of recursive splitting, which means the overall complexity is driven by the exponential growth in results.
-
-Thus, the time complexity of the algorithm is O(n⋅2 
-n
- ), where the O(2 
-n
- ) factor reflects the exponential growth in the number of ways to combine results from sub-expressions.
-
-Space complexity: O(2 
-n
- )
-
-The algorithm stores the intermediate results at each step. Since the total number of results can be equal to the O(2 
-n
- ), the space complexity of the algorithm is O(2 
-n
- ).
-
-*/
-
 class Solution {
 public:
+    vector<vector<vector<int>>> mp;
     vector<int> diffWaysToCompute(string expression) {
         int n = expression.size();
+        mp.resize(n,vector<vector<int>>(n));
         vector<int> ans;
-        // Base case: if the string is empty, return an empty list
-        if (expression.length() == 0) return ans;
+        ans = solve(expression, 0, n-1);
+        return ans;
+    }
 
-        // Base case: if the string is a single character, treat it as a number
-        // and return it
-        if (expression.length() == 1) {
-            ans.push_back(stoi(expression));
-            return ans;
+    vector<int> solve(string s, int l, int r){
+        if(l == r)return {s[l] - '0'};
+        else if(l > r)return {0};
+        else if(r-l == 1 && isdigit(s[l])){
+            int tens = s[l] - '0';
+            int ones = s[r] - '0';
+            return {10 * tens + ones};
         }
 
-        // If the string has only two characters and the first character is a
-        // digit, parse it as a number
-        if (expression.length() == 2 && isdigit(expression[0])) {
-            ans.push_back(stoi(expression));
-            return ans;
-        }
+        if(!mp[l][r].empty())return mp[l][r];
 
-        for(int i = 0; i < n; i++){
-            char c = expression[i];
-            if(c >= '0' && c <= '9')continue;
-            
-            vector<int> left = diffWaysToCompute(expression.substr(0,i));
-            vector<int> right = diffWaysToCompute(expression.substr(i+1));
+        vector<int> temp;
+        for(int i = l; i <= r; i++){
+            if(isdigit(s[i]))continue;
 
-            for(int x: left){
-                for(int y: right){
-                    if(c == '+')ans.push_back(x + y);
-                    else if(c == '*')ans.push_back(x * y);
-                    else ans.push_back(x - y);
+            vector<int> left = solve(s, l, i-1);
+            vector<int> right = solve(s, i+1, r);
+
+            for(auto x: left){
+                for(auto y: right){
+                    if(s[i] == '+')temp.push_back(x+y);
+                    else if(s[i] == '-')temp.push_back(x-y);
+                    else temp.push_back(x*y);
                 }
             }
         }
-        return ans;
+        return mp[l][r] = temp;
     }
 };
