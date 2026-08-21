2a30ba56f8a28ab3a5b7ceb853eb0d6a4f6d3ec7
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-08-21 15:49:39 +0530

    Time: 22 ms (98.89%) | Memory: 16.7 MB (45.47%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int ans = 0, n = nums.size();
-        for(int i = 0; i < n-2; i++){
-            for(int j = i+1; j < n-1; j++){
-                int s = nums[i] + nums[j];
-                int d = abs(nums[i]-nums[j]);
-                // cout<<nums[i]<<" "<<nums[j]<<"  s- "<<s<<" d- "<<d<<"   ";
-                int r = lower_bound(nums.begin()+j+1, nums.end(), s) - nums.begin();
-                int l = upper_bound(nums.begin()+j+1, nums.end(), d) - nums.begin();
-                // cout<<r<<" "<<l<<endl;
-                ans += max(0,r-l);
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        for(int k = 2; k < nums.size(); k++){
+            int longest = nums[k];
+            int i = 0, j = k-1;
+            while(i < j){
+                if(nums[i] + nums[j] > longest){
+                    ans += (j-i);
+                    j--;
+                }
+                else i++;    
             }
         }
         return ans;
