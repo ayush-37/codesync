ca79c8e674532fccedf5f746fec3abe02c34495d
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-08-22 22:07:34 +0530

    Time: 8 ms (5.72%) | Memory: 14.2 MB (6.61%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.cpp
+++ 1095-two-city-scheduling/two-city-scheduling.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    using ll = long long;
-    vector<vector<vector<ll>>> v;
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        int n = costs.size()/2;
-        v.resize(2*n+1, vector<vector<ll>>(n+1,vector<ll>(n+1,-1)));
-        return solve(0,n,n,costs);
-    }
-    ll solve(int i, int a, int b, vector<vector<int>>& costs){
-        if(a == 0 && b == 0)return 0;
-        if(a < 0 || b < 0)return INT_MAX;
-        if(v[i][a][b] != -1)return v[i][a][b];
-        ll at = INT_MAX, bt = INT_MAX;
-        if(a != 0)at = costs[i][0] + solve(i+1, a-1, b, costs);
-        if(b != 0)bt = costs[i][1] + solve(i+1, a, b-1, costs);
+        auto cmp = [&](vector<int> a, vector<int> b){
+            return a[1]-a[0] <= b[1]-b[0];
+        };
 
-        return v[i][a][b] = min(at,bt);
+        sort(costs.begin(), costs.end(), cmp);
+
+        int ans = 0;
+        for(int i = 0; i < costs.size(); i++){
+            if(i < costs.size()/2)ans += costs[i][1];
+            else ans += costs[i][0];
+        }
+
+        return ans;
     }
 };
