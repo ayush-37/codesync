81cf345ac5ca38178623d506c964b4e6a7fa5871
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-09-13 17:07:42 +0530

    Time: 48 ms (64.80%) | Memory: 12.7 MB (98.21%) - LeetSync

--- 864-image-overlap/image-overlap.cpp
+++ 864-image-overlap/image-overlap.cpp
@@ -1,52 +1,39 @@
 class Solution {
 public:
-    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+    int largestOverlap(vector<vector<int>>& img1,
+                       vector<vector<int>>& img2) {
+
         int n = img1.size();
         int ans = 0;
-        for(int r = 0; r < n; r++){
-            for(int c = 0; c < n; c++){
+
+        for (int r = -(n - 1); r <= n - 1; r++) {
+            for (int c = -(n - 1); c <= n - 1; c++) {
+
                 int temp = 0;
-                for(int i = 0; i+r < n; i++){
-                    for(int j = 0; j+c < n; j++){
-                        if((img1[i][j] == 1) && (img1[i][j] == img2[i+r][j+c]))temp++;
+
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+
+                        int ni = i + r;
+                        int nj = j + c;
+
+                        // Check whether shifted position
+                        // is inside img2
+                        if (ni >= 0 && ni < n &&
+                            nj >= 0 && nj < n) {
+
+                            if (img1[i][j] == 1 &&
+                                img2[ni][nj] == 1) {
+                                temp++;
+                            }
+                        }
                     }
                 }
+
                 ans = max(ans, temp);
             }
         }
-        for(int r = 0; r < n; r++){
-            for(int c = 0; c < n; c++){
-                int temp = 0;
-                for(int i = 0; i+r < n; i++){
-                    for(int j = n-1; j-c >= 0; j--){
-                        if((img1[i][j] == 1) && (img1[i][j] == img2[i+r][j-c]))temp++;
-                    }
-                }
-                ans = max(ans, temp);
-            }
-        }
-        for(int r = 0; r < n; r++){
-            for(int c = 0; c < n; c++){
-                int temp = 0;
-                for(int i = n-1; i-r >= 0; i--){
-                    for(int j = 0; j+c < n; j++){
-                        if((img1[i][j] == 1) && (img1[i][j] == img2[i-r][j+c]))temp++;
-                    }
-                }
-                ans = max(ans, temp);
-            }
-        }
-        for(int r = 0; r < n; r++){
-            for(int c = 0; c < n; c++){
-                int temp = 0;
-                for(int i = n-1; i-r >= 0; i--){
-                    for(int j = n-1; j-c >= 0; j--){
-                        if((img1[i][j] == 1) && (img1[i][j] == img2[i-r][j-c]))temp++;
-                    }
-                }
-                ans = max(ans, temp);
-            }
-        }
+
         return ans;
     }
 };
