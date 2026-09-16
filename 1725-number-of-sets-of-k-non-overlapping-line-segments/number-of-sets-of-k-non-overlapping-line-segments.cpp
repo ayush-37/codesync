bfbdecd376f1672afc67036b450ad66338bc8009
ayush-37 <138144648+ayush-37@users.noreply.github.com>
2026-09-16 13:54:58 +0530

    Time: 66 ms (49.53%) | Memory: 54.5 MB (28.04%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     const int MOD = 1e9 + 7;
-    vector<vector<int>> memo;
+    vector<vector<int>> memo, suffix;
     int numberOfSets(int n, int k) {
         memo.resize(n+1, vector<int>(k+1,-1));
+        suffix.assign(n + 1, vector<int>(k + 1, -1));
         return solve(0,n,k);
     }
     int solve(int i, int n, int k){
@@ -19,11 +20,23 @@ public:
         if(memo[i][k] != -1)return memo[i][k];
 
         int take = 0, skip = 0;
-        for(int j = i+1; j < n; j++){
-            take = (take + solve(j,n,k-1)) % MOD;
-        }
+        take = getSuffix(i + 1, n, k - 1);
         skip = solve(i+1, n, k);
 
         return memo[i][k] = (take + skip) % MOD;
+    }
+
+    int getSuffix(int i, int n, int k) {
+
+        if(i >= n)
+            return 0;
+
+        if(suffix[i][k] != -1)
+            return suffix[i][k];
+
+        int current = solve(i, n, k);
+        int remaining = getSuffix(i + 1, n, k);
+
+        return suffix[i][k] = (current + remaining) % MOD;
     }
 };
